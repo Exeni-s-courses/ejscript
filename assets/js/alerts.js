737af0bb773ad50b9de707ee5772c12c16cd8190
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-01-23 17:26:27 -0400

    letcode code

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -1,16 +1,29 @@
-var reduce = function (nums, fn, init) {
-  if (nums.length == 0) return init;
-  let val = 0;
-  nums.forEach((element) => {
-    val = fn(init, element);
-    init = val;
-  });
-  return val;
-};
-const arr = [1, 2, 3, 4];
-const init = 0;
-const fn = function sum(accum, curr) {
-  return accum + curr;
-};
-const res = reduce(arr, fn, init);
-console.log(res);
+function memoize(fn) {
+  var state;
+  return function (...args) {
+    const res = fn(...args)
+    console.log(args, state)
+    // Esto revisa una vez
+    if (state && JSON.stringify(state.args) === JSON.stringify(args)) return state.res;
+    console.log('once')
+    state = { args: [...args], res }
+    return fn(...args)
+  }
+}
+
+// const sum = (a, b) => a + b;
+// const memoizedSum = memoize(sum);
+// console.log(memoizedSum(2, 2)); // "call" - devuelve 4. Se llama a sum() porque (2, 2) no se había visto antes.
+// console.log(memoizedSum(2, 2)); // "call" - devuelve 4. Pero sum() no se llamó porque los mismos argumentos ya se habían visto.
+// // "getCallCount" - número total de llamadas: 1
+// console.log(memoizedSum(1, 2)); // "call" - devuelve 3. Se llama a sum() porque (1, 2) no se había visto antes.
+// // "getCallCount" - número total de llamadas: 2
+
+const factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1));
+const memoFactorial = memoize(factorial);
+console.log(memoFactorial(2)); // "call" - devuelve 2.
+console.log(memoFactorial(3)); // "call" - devuelve 6.
+console.log(memoFactorial(2)); // "call" - devuelve 2. Pero factorial no se llamó porque 2 ya se había visto antes.
+// "getCallCount" - número total de llamadas: 2
+console.log(memoFactorial(3)); // "call" - devuelve 6. Pero factorial no se llamó porque 3 ya se había visto antes.
+// "getCallCount" - número total de llamadas: 2
