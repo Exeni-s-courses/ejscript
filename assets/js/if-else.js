a6ca92ac0b47ac30b7c71218bf2d96fb49d9addc
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-01-24 13:17:42 -0400

    else if lesson

---
+++ assets/js/if-else.js
@@ -0,0 +1,34 @@
+let a = 8;
+
+// if (a >= 10) {
+//     console.log('A es mayor o igual a 10');
+// } else {
+//     console.log('A es menor a 10')
+// }
+// console.log('Fin de programa');
+
+
+// const hoy = new Date();
+
+// let dia = hoy.getDay();
+
+// console.log(dia)
+
+// if (dia === 0) {
+//     console.log('Domingo');
+// } else {
+//     console.log('No es Domingo')
+// }
+
+dia = 3;
+const semana = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miercoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sabado'
+}
+
+console.log(semana[dia])
